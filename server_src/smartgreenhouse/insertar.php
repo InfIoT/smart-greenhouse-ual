166c9a80331a5656a9e93de73ca4f7d88c2badd2
Claudio Peña Rodríguez <claudio_adra@hotmail.com>
2018-04-22 20:59:43 +0200

    Update insertar.php

--- server_src/smartgreenhouse/insertar.php
+++ server_src/smartgreenhouse/insertar.php
@@ -1,7 +1,7 @@
 <?php
 include("conexion.php");
 
-// recibir datos
+// Recepción de datos
 $BMETEMP=$_POST['BMETEMP'];
 $BMEPRES=$_POST['BMEPRES'];
 $BMEHUM=$_POST['BMEHUM'];
@@ -41,41 +41,17 @@ $calefaccion=0;
 }
 
 
-
-// procesar
-//if($temp_interna > 35)
-//$ventana=20;
-//if($hum_aire_int < 20)
-//$riego=1;
-//$ventilacion=$_POST['ventilacion'];
-//$calefaccion=$_POST['calefaccion'];
-
-
-//if(isset($_GET['temp_externa']) && !empty($_GET['temp_externa']) && isset($_GET['hum_aire_ext']) //&& !empty($_GET['hum_aire_ext']))
-//{
-
-// conexion a la bd
+// Conexión a la Base de Datos
 $con=mysql_connect($host,$user,$pw)or die("problemas al conectar");
 mysql_select_db($db,$con)or die("problemas al conectar la bd");
 
 
-//echo mysql_query("INSERT INTO datos (TEMP_EXTERNA,HUM_AIRE_EXT) VALUES ('$_GET
-//['temp_externa']','$_GET['hum_aire_ext']')",$con);
-
+// Inserción a la Base de Datos
 mysql_query("INSERT INTO datos(BMETEMP, BMEPRES, BMEHUM, HUMSUELO, TEMPSUELO, TEMPEXT, PRESEXT, HUMEXT, LUMINOSIDAD, ventana, riego, calefaccion, fechaHora) VALUES ('$BMETEMP','$BMEPRES','$BMEHUM','$HUMSUELO','$TEMPSUELO','$TEMPEXT','$PRESEXT','$HUMEXT',
 '$LUMINOSIDAD','$ventana','$riego','$calefaccion','$fechaHora')") or die("error");
 
-// actuadores
 
-//$ventana=20;
-//if($hum_aire_int < 20)
-//$riego=1;
-//$ventilacion=$_POST['ventilacion'];
-//$calefaccion=$_POST['calefaccion'];
-//ifs entonces actúo GPIO no se qué
-
-
-
+// Actuadores
 if ($ventana==1) { 
    if(($BMETEMP > 20)and($BMETEMP < 29)){
 	$a- exec("sudo python /var/www/html/smartgreenhouse/abrirVentanaMedio.py");
